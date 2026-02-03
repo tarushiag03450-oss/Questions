0f3c685b1a654f8a2db8f4dc3dab2b7811dcbca0
Tarushi Agrawal <tarushiag03450@gmail.com>
2026-02-03 19:55:48 +0530

    Update countEvens.java

--- countEvens.java
+++ countEvens.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 class UserMain{
   public int countEvens(int input1,int input2,int input3,int input4,int input5{
-    int count==0;
+    int count=0;
     if(input1%2==0) count++;
     if(input2%2==0) count++;
     if(input3%2==0) count++;
