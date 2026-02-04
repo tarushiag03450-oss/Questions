f20132f3e0eba094d838e2f0ffbce9d03745523b
Tarushi Agrawal <tarushiag03450@gmail.com>
2026-02-04 19:21:50 +0530

    Create nthPrime.java

---
+++ nthPrime.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class UserMain{
+  public int nthPrime(int input1){
+    int count=0;
+    int num=2;
+    while(true){
+      boolean isPrime=true;
+      for(int i=2;i*i<=num;i++){
+        if(num%i==0){
+          isPrime=false;
+          break;
+        }
+      }
+      if(isPrime){
+        count++;
+        if(count==n){
+          return num;
+        }
+      }
+      num++;
+    }
+  }
+}
