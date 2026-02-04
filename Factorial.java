f6393cb8e53df7a7604fea9d243564ccde24efee
Tarushi Agrawal <tarushiag03450@gmail.com>
2026-02-04 19:00:18 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class UserMain{
+  public int factorial(int input1){
+  int input1=1;  
+  for(int i=0;i<=input1;i++){
+      input1=input1*i;
+  }
+  return input1;
+}
