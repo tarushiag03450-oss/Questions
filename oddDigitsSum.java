8069bc49fd747f7a58e543bf3478349b0a02edc4
Tarushi Agrawal <tarushiag03450@gmail.com>
2026-02-06 20:44:51 +0530

    Create oddDigitsSum.java

---
+++ oddDigitsSum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode{
+	public int OddDigitsSum(int input1,char input2){
+		int e=0,d=0;
+    	while(input1>0){
+        if((input1%10)%2==0) e+=(input1%10);
+        else d+=(input1%10);
+        input1/=10;
+    	}
+    	if(input2.equals("even")) return e;
+    	else return d;
+    }
+}
